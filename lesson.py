739eb2e7616f6c1128e227ec1a8130bc919aee7a
Michael-Franklyn-MDF <michaeldavidfranklyn@gmail.com>
2025-12-08 11:04:38 +0300

    Practice Run

---
+++ lesson.py
@@ -0,0 +1,127 @@
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# print(even_or_odd(10))
+# print(even_or_odd(11))
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# def age_increase(age):
+#     return age + 1
+
+# print(f"Hello {name}, Next year you will be {age_increase(age)} years old.")
+
+# number = float(input("Enter a number: "))
+
+# def num_check(number):
+#     if number > 0:
+#         return "Positive"
+#     elif number < 0:
+#         return "Negative"
+#     else:
+#         return "Zero"
+
+# print(f"The number {number} is {num_check(number)}.")
+
+   
+# for i in range(1, 20):
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         print(" ")
+
+# num = int(input("Enter a number: "))
+
+# def sqaure(num):
+#     return num * num
+
+# num = int(input("Enter a number: "))
+
+# def square(num1):
+#     return num1 * num1
+
+# num = int(input("Enter a number: "))
+
+# def square(num2):
+#     return num2 * num2
+
+# num = int(input("Enter a number: "))
+
+# def square(num3):
+#     return num3 * num3
+
+# num = int(input("Enter a number: "))
+
+# def square(num4):
+#     return num4 * num4  
+
+# print(f"The square of {num} is {sqaure(num)}.")
+# print(f"The square of {num1} is {square(num1)}.")
+# print(f"The square of {num2} is {square(num2)}.")
+# print(f"The square of {num3} is {square(num3)}.")
+# print(f"The square of {num4} is {square(num4)}.")
+
+
+# fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+# for i in range(len(fruits)):
+#     print("Fruit",i, ":", fruits[i])
+
+
+# fruits = ["apple", "banana"]
+
+# fruits.append("mango")
+# fruits.append("orange")
+
+# for fruit in fruits:
+#     print(fruit)
+
+# colors = ["red", "blue", "yellow"]
+
+# colors.insert(1, "green")
+# colors.insert(3, "purple")
+
+# for color in colors:
+#     print(color)
+
+# items = ["phone", "laptop", "tablet", "camera"]
+
+# items.remove("tablet")
+# items.pop(2)
+
+# for item in items:
+#     print(item)
+
+# numbers = [5, 1, 9, 3, 7]
+
+# numbers.sort()
+# numbers.reverse()
+
+# for number in numbers:
+#     print(number)
+
+# nums = [2, 4, 2, 6, 2, 8]
+
+# nums.count(2)
+# nums.index(6)
+
+# print(nums.count(2))
+# print(nums.index(6))
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+
+# c = a + b
+
+# for num in c:
+#     print(num)
+
+students = ["Mike", "John", "Sarah"]
+
+students.clear()
+
+print(students)
